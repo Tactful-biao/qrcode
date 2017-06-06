1f51d247a03431c648afd5f6adb43e6b61ca4375
Tactful-biao <1208339113@qq.com>
2017-06-06 18:55:55 +0800

    二维码生成器代码

---
+++ qrcode.py
@@ -0,0 +1,64 @@
+##########################################
+# File:             util.py
+# Author:           孙士标
+##########################################
+
+from lib.qrgenerator import generate
+from lib.qrscanner import scan
+
+if __name__ == '__main__':
+    import sys
+
+    class InvalidArgs(Exception):
+        """Invalid arguments passed in."""
+        def __init__(self, arg):
+            self.arg = arg
+
+        def __str__(self):
+            return repr(self.arg)
+
+    try:
+        if len(sys.argv) < 2:
+            raise InvalidArgs('Invalid arguments')
+        if sys.argv[1] == '-g':
+            width = 210
+            filename = 'qrcode.jpg'
+            try:
+                idxw = sys.argv.index('-w')
+            except ValueError:
+                pass
+            else:
+                try:
+                    width = int(sys.argv[idxw+1])
+                except Exception:
+                    raise InvalidArgs('Invalid arguments')
+                sys.argv.remove('-w')
+                sys.argv.remove(str(width))
+            try:
+                idxf = sys.argv.index('-f')
+            except ValueError:
+                pass
+            else:
+                try:
+                    filename = sys.argv[idxf+1]
+                except Exception:
+                    raise InvalidArgs('Invalid arguments')
+                sys.argv.remove('-f')
+                sys.argv.remove(filename)
+            if len(sys.argv) == 3:
+                generate(sys.argv[2], width, filename)
+            else:
+                raise InvalidArgs('Invalid arguments')
+        elif sys.argv[1] == '-s':
+            if len(sys.argv) == 3:
+                print(scan(filename=sys.argv[2]))
+            else:
+                raise InvalidArgs('Invalid arguments')
+        else:
+            raise InvalidArgs('Invalid arguments')
+    except InvalidArgs:
+        print('Usage:\n' +
+              'Generate: python qrcode.py -g data [-w width] [-f filename]\n' +
+              'Scan: python qrcode.py -s filename')
+    except Exception as e:
+        print(e)
---
+++ qrcode_example.py
@@ -0,0 +1,13 @@
+import qrcode
+
+qrcode.generate('JL16110006')
+
+qrcode.generate('Python QR code!', width=420, filename='example.jpg')
+
+qrcode.scan('qrcode.jpg')
+
+qrcode.scan('example.jpg')
+
+# Exception raised here, as version 1 QR code cannot encode
+# more than 17 characters in byte mode.
+qrcode.generate('012345678901234567')
